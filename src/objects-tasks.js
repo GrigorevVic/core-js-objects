ef0eae5a3b61b53d1b474850ddbd2189a522d716
Grigorev Viktor <4342718@gmail.com>
2024-03-03 23:22:01 +0300

    feat: 16 tests passing

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -200,8 +200,14 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const newProperty = JSON.parse(json);
+  const newObject = Object.create(proto);
+  const keys = Object.keys(newProperty);
+  keys.forEach((key) => {
+    newObject[key] = newProperty[key];
+  });
+  return newObject;
 }
 
 /**
